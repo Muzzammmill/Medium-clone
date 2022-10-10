21c7362dd250c19bfc4f3572eb64bbc5bab1a18a
Idris Muzzammil Ayobami <idrisking759@gmail.com>
2022-10-10 16:34:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const articles = document.querySelectorAll(".article")
+const articleCont = document.querySelector(".article-wrapper")
+const nav = document.querySelector("nav")
+const navBtn = document.querySelector(".btn1")
+
+function loadNewArticle() {
+  for (let article of articles) {
+    const newArticle = article.cloneNode(true)
+    articleCont.appendChild(newArticle)
+  }
+}
+
+const lastArticleObserver = new IntersectionObserver(entries => {
+  const lastArticle = entries[0]
+  if (lastArticle.isIntersecting)
+    return loadNewArticle()
+  lastArticleObserver.unobserve(lastArticle.target)
+  lastArticleObserver.observe(document.querySelector(".article:last-child"))
+})
+
+lastArticleObserver.observe(document.querySelector(".article-flex:last-child"))
+
+
+console.log(articles);
+
+
+const navObserver = new IntersectionObserver(entry => {
+  if (entry.isIntersecting) {
+    nav.style.backgroundColor = "white";
+    navBtn.style.backgroundColor = "green"
+  }
+  // },
+  //   {
+  //     threshold: 1,
+})
+navObserver.observe(nav)
